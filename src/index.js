3b5c6cdbef13117a78b263f9292720bd1a84e4ab
unknown <uljasha.sav@gmail.com>
2024-10-02 18:19:07 +0600

    implement deleting todos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-const todosListValues = [];
+const todosListValues = JSON.parse(localStorage.getItem('list')) || ['Тестовая'];
 const addForm = document.getElementById("addForm");
 const todoInput = document.querySelector('.todoInp');
 const list = document.querySelector('.list');
+const deleteBtns = document.querySelectorAll('.list');
 
 addForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -12,37 +13,54 @@ addForm.addEventListener('submit', (e) => {
     }
 });
 
+deleteBtns.forEach((deleteBtn) => {
+    deleteBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        const id = e.target.parentNode.value;
+        if (id > -1) {
+            deleteItem(id);
+        }
+    });
+})
 
 const addItem = (item) => {
     todosListValues.push(item);
+    updateStorage('list', JSON.stringify(todosListValues));
     generateList();
 }
 
 const editItem = (item) => {
-    todosListValues.push(item);
+    console.log(123);
 }
 
-const deleteItem = (item) => {
-    todosListValues.filter((itemInArr) => itemInArr !== item)
+const deleteItem = (id) => {
+    todosListValues.splice(id, 1);
+    updateStorage('list', JSON.stringify(todosListValues));
+    generateList();
+}
+
+const updateStorage = (id, data) => {
+    localStorage.setItem(id, data)
 }
 
 const generateList = () => {
     list.innerHTML = '';
 
-    todosListValues.forEach(item => {
+    todosListValues.forEach((item, index) => {
         const listItem = document.createElement('li');
         listItem.classList.add('list__item');
+        listItem.value = index;
 
         const listItemText = document.createElement('div');
         listItemText.classList.add('list__item__text');
         listItemText.textContent = item;
 
         const editBtn = document.createElement('div');
-        editBtn.classList.add('btn');
+        editBtn.classList.add('btn', 'editBtn');
         editBtn.textContent = '✎';
 
         const deleteBtn = document.createElement('div');
-        deleteBtn.classList.add('btn');
+        deleteBtn.classList.add('btn', 'deleteBtn');
         deleteBtn.textContent = '✖';
 
         listItem.appendChild(listItemText);
@@ -52,3 +70,5 @@ const generateList = () => {
         list.appendChild(listItem);
     });
 }
+
+generateList();
