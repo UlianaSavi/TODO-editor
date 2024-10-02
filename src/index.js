9f5bceb064662f9973bdd1e2b37038a770f6bf3b
unknown <uljasha.sav@gmail.com>
2024-10-02 17:49:58 +0600

    implement genetaring todos

--- src/index.js
+++ src/index.js
@@ -1,14 +1,54 @@
-const todosList = [];
+const todosListValues = [];
+const addForm = document.getElementById("addForm");
+const todoInput = document.querySelector('.todoInp');
+const list = document.querySelector('.list');
 
-const add = (item) => {
-    todosList.push(item);
-}
-
-my_element.addEventListener("click", function (e) {
-    console.log(this.className); // logs the className of my_element
-    console.log(e.currentTarget === this); // logs `true`
+addForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const inputValue = todoInput.value;
+    todoInput.value = '';
+    if (inputValue) {
+        addItem(inputValue)
+    }
 });
 
-const generateList = (list) => {
-    
+
+const addItem = (item) => {
+    todosListValues.push(item);
+    generateList();
+}
+
+const editItem = (item) => {
+    todosListValues.push(item);
+}
+
+const deleteItem = (item) => {
+    todosListValues.filter((itemInArr) => itemInArr !== item)
+}
+
+const generateList = () => {
+    list.innerHTML = '';
+
+    todosListValues.forEach(item => {
+        const listItem = document.createElement('li');
+        listItem.classList.add('list__item');
+
+        const listItemText = document.createElement('div');
+        listItemText.classList.add('list__item__text');
+        listItemText.textContent = item;
+
+        const editBtn = document.createElement('div');
+        editBtn.classList.add('btn');
+        editBtn.textContent = '✎';
+
+        const deleteBtn = document.createElement('div');
+        deleteBtn.classList.add('btn');
+        deleteBtn.textContent = '✖';
+
+        listItem.appendChild(listItemText);
+        listItem.appendChild(editBtn);
+        listItem.appendChild(deleteBtn);
+
+        list.appendChild(listItem);
+    });
 }
